f6c642a78c84a0eb7fd8f65dbd8248a71eaea028
SleepyBear1 <47032020+SleepyBear1@users.noreply.github.com>
2020-06-03 20:35:30 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const precess_db = require('dotenv').config();
 const db_url = process.env.DATABASE_URL || process_db.parsed.DB_URL;
-const client = new pg.client({
+const client = new pg.Client({
     connectionString: db_url,
     ssl: { rejectUnauthorized: false }
 });
